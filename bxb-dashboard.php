d35b03c76e665c344e48f7fea2339ae52ce78178
CVLTIK <zlovlokvi@gmail.com>
2025-04-02 17:37:17 -0700

    Activation

--- bxb-dashboard.php
+++ bxb-dashboard.php
@@ -24,8 +24,8 @@ define('BXB_dashboard_URL', plugin_dir_url(__FILE__));
 // Include required files.
 // require_once BXB_dashboard_DIR . 'includes/admin.php';
 require_once BXB_dashboard_DIR . 'includes/enqueue.php';
-require_once BXB_dashboard_DIR . 'includes/acf-fields.php';
-require_once BXB_dashboard_DIR . 'includes/settings.php';
+// require_once BXB_dashboard_DIR . 'includes/acf-fields.php';
+// require_once BXB_dashboard_DIR . 'includes/settings.php';
 
 
 
